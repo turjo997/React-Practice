2a88490c6c019246a5e1a8d085f333806db9f8f7
turjo998 <ullash.bhattacharjee@bjitacademy.com>
2023-06-17 20:11:32 +0600

    updated

--- react-rest-countries-updated/src/components/Countries/Countries.js
+++ react-rest-countries-updated/src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -15,12 +16,17 @@ const Countries = () => {
     return (
         <div>
             <h2>Hello Countries: {countries.length}</h2>
-            {
-                countries.map(country =>
-                    <Country name={country.name.common}
-                        area={country.area}
-                        population={country.population}></Country>)
-            }
+            <div className='countries-container'>
+                {
+                    countries.map(country =>
+                        <Country
+                            country={country}
+                        // key={country.cca3}
+                        >
+                        </Country>)
+                }
+            </div>
+
 
 
         </div>
--- react-rest-countries-updated/src/components/Country/Country.js
+++ react-rest-countries-updated/src/components/Country/Country.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    const { name, area, region, population, flags } = props.country;
     return (
         <div className='country'>
-            <h2>Name: {props.name}</h2>
-            <p>Area: {props.area}</p>
-            <p>Population: {props.population}</p>
+
+            <h2>Name: {name.common}</h2>
+            <img src={flags.png} alt='' />
+            <p>Area: {area}</p>
+            <p>Region: {region}</p>
+            <p>Population: {population}</p>
         </div>
     );
 };
