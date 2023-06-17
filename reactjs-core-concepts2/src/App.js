c4aaa4b59217cf4b2a816e2a86fc439ffaa46665
turjo998 <ullash.bhattacharjee@bjitacademy.com>
2023-06-17 11:10:23 +0600

    core concepts2 added

--- reactjs-core-concepts2/src/App.js
+++ reactjs-core-concepts2/src/App.js
@@ -1,33 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    { name: 'laptop', price: '47000' },
-    { name: 'phone', price: '76000' },
-    { name: 'watch', price: '15000' },
-    { name: 'TV', price: '100000' }
-  ]
+
   return (
     <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-      {/* <Product name="laptop" price="47000"></Product>
-      <Product name="phone" price="75000"></Product>
-      <Product name="watch" price="20000"></Product> */}
+      <Counter></Counter>
 
     </div>
   );
 }
 
-function Product(props) {
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+
+  // const increaseCount = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // }
+
   return (
-    <div className="product">
-      <h3>Name: {props.name}</h3>
-      <p>Price: {props.price}</p>
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   )
 }
+
 
 export default App;
